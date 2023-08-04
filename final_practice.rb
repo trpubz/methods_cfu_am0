639c20a3813655a7b9a4aa9ed5836094e7f060e9
Taylor Pubins <tpubz@icloud.com>
2023-08-04 15:22:49 -0700

    Complete final practice

--- final_practice.rb
+++ final_practice.rb
@@ -1,40 +1,74 @@
 # Defining Our Own Methods
-# For each exercise below, write the method according to the requirements. 
-# Call each method at least twice and store the return value in a variable. 
+# For each exercise below, write the method according to the requirements.
+# Call each method at least twice and store the return value in a variable.
 # Use the puts or print command to see the return value in the console.
 
 
-# 1: Write a method named greeting that returns a string with a general greeting. 
-
-# What is the return value of your method?
-# How many arguments did you pass your method?
+# 1: Write a method named greeting that returns a string with a general greeting.
+def greeting
+  "Hola, world"
+end
+greeting1 = greeting
+greeting2 = greeting
+puts greeting1
+puts greeting2
+# What is the return value of your method? => "Hello, world
+# How many arguments did you pass your method? => 0
 
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
-
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+def custom_greeting(name)
+  "Hola, #{name}"
+end
+greet_taylor = custom_greeting("taylor")
+greet_amanda = custom_greeting("amanda")
+puts greet_taylor
+puts greet_amanda
+# What is the return value of your method? => "Hola, <name>"
+# How many arguments did you pass your method? => 1
+# What data type was your argument(s)? => string
 
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
-
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+def greet_person(fname, mname, lname)
+  "Hola, #{fname.capitalize} #{mname.capitalize} #{lname.capitalize}"
+end
+greet_taylor = greet_person("taylor", "reid", "pubins")
+greet_amanda = greet_person("amanda", "mae", "pubins")
+puts greet_taylor
+puts greet_amanda
+# What is the return value of your method? =>  "Hola, <fname> <mname> <lname>""
+# How many arguments did you pass your method? => 3
+# What data type was your argument(s)? => string
 
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
-
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+def square(int)
+  result = Integer::sqrt(int)
+  puts "The square of #{int} equals #{result}"
+  result
+end
+sqr1 = square(4)
+sqr2 = square(16)
+# What is the return value of your method? => 2, 4
+# How many arguments did you pass your method? => 1
+# What data type was your argument(s)? => integer
 
 
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
 
+
+def check_stock(qty, item)
+  if qty >= 4
+    puts "#{item} is stocked"
+  elsif qty == 0
+    puts "#{item} - OUT of stock!"
+  else
+    puts "#{item} - running LOW"
+  end
+end
 check_stock(4, "Coffee");
 # => "Coffee is stocked"
 
--- self_eval.md
+++ self_eval.md
@@ -1,4 +1,6 @@
 # Self-Evaluation
 
 - Reflect on your learning and work today. What are you most proud of?
+    - using source docs efficiently
 - What is the power of using methods in our applications?
+    - resuability and adhearance to the SOLID principles
